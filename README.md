96f431daf641cd6d67f99181dd3e33d011379c0a
Eric Bouillet <eric.bouillet@epfl.ch>
2025-02-01 14:39:42 +0100

    wip: first commit

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
-# crm-merger
+# User Profile Merger
+
 Consolidate user profiles for event campaign coordination
+
+## Usage
+
+```
+./merge.py --list *.csv  --rules rules.json --encoding 'iso-8859-1' --out out.csv --log debug --logfile out.log
+
+--list: a list of input csv files to be merged.
+--out: the output of the merged files (default to terminal).
+--rules: an optional json file with a set of data processing rules.
+--encoding: optional default encoding, if not specified the tool will try to detect the most likely encoding.
+--log: log level (debug, info, warning, error, critical).
+--logfile: log output (default to terminal).
+```
+
+## Rule set
+
+Transformation rules are expressed in the JSON rule file. The rules consist of five main sections, other sections are ignored:
+
+- Encode: a default encoding used for the files.
+- Rename: set of regex substitution rules used to rename the table headers.
+- Transform: set of regex substitution rules used to convert individual record values.
+- Validate: set of predefined rules or regex substitution rules used to validate individual record values.
+- Custom: set of custom actions, used to invoke registered python code on given fields.
+
+### Encoding rules
+
+Specify the _encoding_ of files that matches the given regex _file_ pattern.
+Patterns must matches from the start of the file path name.
+
+```
+{
+    "encode": [
+       { "encoding": null, "comment": "use null as default to autmatically detect the encoding." },
+       { "file": "(?i:.*\\.utf8)$",   "encoding": "UTF-8" }
+    ],
+}
+```
+
+### Renaming rules
+
+Rename headers matches the given regex _field_ pattern.
+Patterns must matches from the start of the field name.
+
+```
+    "rename": [
+       { "field": "(?i:Name)$", "to": "Names", "comment": "rename Name to Names" }
+    ],
+```
+
+### Transformation rules
+
+Substitute into _to_ values in column _field_ (or all) that match the _re_ pattern.
+Use back references in _to_, e.g. "\\1" to insert group captures.
+
+```
+    "transform": [
+       { "field": "(?i:First Name)", "re": "^(?i:\\s*dr\\.|\\s*prof\\.)+\\s+", "to": "" },
+       { "re": "^\\s+", "to": "" },
+       { "re": "\\s+$", "to": "" }
+    ],
+```
+
+### Validation rules
+
+Ensure that the values in the _field_ column either match the provided _re_ pattern or conform to the predefined type rule
+(in the example below, both rules are equivalent). Validation occurs after the transformation.
+Use the _log_ attribute to specify the log level for invalid entries (setting to 'critical' will raise an uncaught exception).
+
+```
+    "validate": [
+       { "field": "(?i:Email)", "type": "email", "log": "warning", "unique": true, "id": "email:1" },
+       { "field": "(?i:Email)", "re": "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9_.-]+\\.[a-zA-Z0-9_.-]+(\\.[a-zA-Z0-9_.-])?$", "log": "warning", "unique": true, "id": "email:2" }
+    ],
+```
+
+### Custom rules
+
+Perform custom actions on field values that match the _re_ expression (or on all fields if _field_ is null, and all values if _re_ is null).
+Actions registered under the _action_ name in the Python code are invoked with the row, represented as a dictionary, after transformations and validations have been applied.
+
+```
+    "custom": [
+       { "field": "(?i:Email)", "re": "(?i:ethz\\.ch|epfl\\.ch|unil\\.ch)", "action": "academia_Domains", "id": "academia:1" }
+    ],
+```
+
+## Notes
+- The tool preserves gmail aliases such as username+tag@gmail.com, however it ignores the +tag when checking for uniqueness.
+- The tool reports duplicates, but does nothing about them
+- We frequently receive input files that combine records from multiple sources, all merged under a single encoding. As a result, records created with different encodings may appear garbled. The tool attempts to automatically correct these encoding issues, but this process is ad-hoc and has not been thoroughly tested with all possible encoding combinations.
---
+++ merge.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env python3
+
+import argparse
+import csv
+import json
+import logging
+import os
+import pathlib
+import re
+import sqlite3
+import sys
+
+
+
+# Decorator to register functions into a lookup table
+class FunctionRegistry:
+    # Class-level dictionary to store functions
+    function_lookup_table = {}
+
+    @staticmethod
+    def register_function(func):
+        FunctionRegistry.function_lookup_table[func.__name__] = func
+        return func
+
+    @staticmethod
+    def exec(name):
+        return FunctionRegistry.function_lookup_table[name]
+
+@FunctionRegistry.register_function
+def academia_Domains(output):
+    """Add Academic to Domains"""
+    domains = output.get('Domains',None)
+    if domains is None:
+        return
+    domains = [x for x in list(set(output.get('Domains', '').split(','))|set(['Academic'])) if x.strip() != '']
+    domains.sort()
+    domains = ','.join(domains)
+    if domains != output['Domains']:
+        logger.info(f'Action: {output["Email Address"]} {output["Domains"]} -> {domains}')
+    output['Domains'] = domains
+
+## Open a file from path or duplicate a file descriptor (e.g. stdout)
+def open_file(f, **kwargs):
+    if isinstance(f, ( str, pathlib.Path )):
+        return open(f, **kwargs)
+    elif isinstance(f, int):
+        return os.fdopen(f, **kwargs)
+    else:
+        raise ValueError(f"f {type(f)} must be either a file path (str) or a file descriptor (int).")
+
+## Merge list b into a, trying to preserve order of a as much as possible
+def merge_list(a, b):
+    if len(a) == 0:
+       return b
+    new_set = set(b) - set(a)
+    merged_list = a
+    b_i = 0
+
+    while len(new_set) > 0:
+        for i,m in enumerate(b[b_i:]):
+            if m in new_set:
+                b_i += i + 1
+                after = set(b[b_i:])
+                new_set -= {m}
+                break
+        if len(after) > 0:
+            for i,n in enumerate(merged_list):
+                if n in after:
+                    break
+            merged_list = merged_list[:i] + [m] + merged_list[i:]
+        else:
+            merged_list = merged_list + [m]
+
+    assert(len(set(a+b) - set(merged_list)) == 0)
+        
+    return merged_list
+
+## Validate value based on type and return True/False result of validation and
+## a canonic value that can be used to check for the unique constraint.
+##
+def validateValue(value, isa):
+    match (isa or '').lower():
+        case 'email':
+            v = re.sub(r'\+.*@gmail', '@gmail', value).lower() # Google ...+...@gmail.com
+            ok = re.match('^[a-zA-Z0-9_.-]+@[a-zA-Z0-9_.-]+\\.[a-zA-Z0-9_.-]+(\\.[a-zA-Z0-9_.-])?$', v)
+        case _:
+            ok = True
+            v = value
+    return v, ok
+
+## Return log level from string
+def getLogLevel(level : str) -> int:
+    return logging.getLevelNamesMapping().get(level.upper())
+
+## Detect encoding of file f
+def detect_encoding(f, rules, args):
+    encoding = args.encoding
+    for r in rules.get('encode', []):
+        if r.get('file') is None:
+           if encoding is None:
+               encoding = r.get('encoding')
+        elif  re.search(r.get('file'), f):
+           encoding = r.get('encoding')
+           break
+    if encoding is None:
+        import chardet
+        with open(f, 'rb') as file:
+            raw_data = file.read()
+            result = chardet.detect(raw_data)
+            encoding = result['encoding']
+            logger.debug(f'{result}')
+    else:
+        logger.debug(f'Encoding: {encoding}')  # This will show you the detected encoding
+    return encoding
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Merge MailChimp lists.')
+    parser.add_argument('--config', '-c', metavar='config', type=pathlib.Path, default='config.ini', help='configuration INI file')
+    parser.add_argument('--list', '-f',  metavar='files', type=str, nargs='+', help='csv files')
+    parser.add_argument('--encoding', '-e', metavar='code', type=str, help='encoding')
+    parser.add_argument('--rules', '-r', metavar='file', type=pathlib.Path, nargs='+', default=[], help='conversion rules')
+    parser.add_argument('--out', '-o', metavar='file', type=pathlib.Path, help='output file')
+    parser.add_argument('--log', '-v', metavar='level', choices=['debug', 'info', 'warn', 'error', 'critical'], default='info', help='log level')
+    parser.add_argument('--logfile', '-O', metavar='file', type=pathlib.Path, help='log file')
+    args = parser.parse_args()
+
+    logger = logging.getLogger(__name__)
+    if args.logfile is not None:
+        logging.basicConfig(filename=args.logfile, filemode='w', format='%(asctime)s %(levelname)s %(message)s', encoding='utf-8', level=logging.INFO)
+    else:
+        logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', encoding='utf-8', level=logging.INFO)
+    logger.setLevel(args.log.upper())
+
+    for r in args.rules:
+        with open(r, 'r') as ruleset:
+            rules = json.load(ruleset)
+
+    ## Merge the column names from all files
+    ## rename the columns according to the rules
+    ## and capitalize each word of the fields and
+    ##
+    logger.info(f'ITERATION 1 -- Merge headers')
+    column_map = dict()
+    fieldnames = []
+    for f in args.list:
+        try:
+            if args.out is not None and os.path.samefile(f, args.out):
+                continue
+        except:
+            pass
+        logger.info(f'FILE {f}')
+        encoding = detect_encoding(f, rules, args)
+
+        with open(f, 'r', encoding = encoding, newline='') as file:
+            csvreader = csv.DictReader(file)
+
+            columns = dict()
+            for c in csvreader.fieldnames:
+                s = c.strip()
+                for r in rules.get('rename', []):
+                    s = re.sub(r['field'], r['to'], s)
+                s = re.sub(r'\b[a-z]', lambda m: m.group(0).upper(), s)
+                if (s != c):
+                    logger.debug(f'Rename field {c} to {s}')
+                    pass
+                columns[c] = s
+
+            axx = set(columns.values()) - set(column_map.values())
+            if (len(axx) > 0):
+                logger.debug(f'ADD  {axx}')
+            mxx = set(column_map.values()) - set(columns.values())
+            if (len(mxx) > 0):
+                logger.debug(f'NOT  {mxx}')
+            column_map |= columns
+            fieldnames = merge_list(fieldnames, [ column_map[c] for c in csvreader.fieldnames ])
+
+    logger.info(f'FIELDS {fieldnames}')
+
+    ## Merge the rows from all files
+    ## Transform and validate the values accordding to the rules
+    ##
+    logger.info(f'ITERATION 2 -- Merge rows')
+    unique_idx = dict()
+    with open_file(args.out or sys.stdout.fileno(), mode='w', newline='') as csvfile:
+        csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        csvwriter.writeheader()
+
+        for f in args.list:
+            if args.out is not None and os.path.samefile(f, args.out):
+                continue
+            logger.info(f'FILE {f}')
+            encoding = detect_encoding(f, rules, args)
+    
+            with open(f, 'r', encoding = encoding, newline='') as file:
+                csvreader = csv.DictReader(file)
+                for row in csvreader:
+                    action_set = set()
+                    logger.debug(f'{"-"*100}')
+                    output = dict()
+                    for field in csvreader.fieldnames:
+                        renamed_field = column_map.get(field)
+                        value=row[field]
+
+                        # Clean the mess caused by people merging inputs of different encodings into same file :-/
+                        try:
+                            value = value.encode('latin1').decode('utf-8')
+                        except (UnicodeDecodeError, TypeError):
+                            try:
+                               value = value.encode('utf-8').decode('utf-8')
+                            except (UnicodeDecodeError, TypeError):
+                               try:
+                                   value = value.encode('iso-8859-1').decode('utf-8')
+                               except (UnicodeDecodeError, TypeError):
+                                   try:
+                                       value = value.encode('windows-1252').decode('utf-8')
+                                   except (UnicodeDecodeError, TypeError):
+                                       logger.warning(f'Re-encoding failed for `{value}`')
+
+                        # Replace field values according to regex rules
+                        for r in rules.get('transform', []):
+                            if r.get('field') is None or re.match(r.get('field'), renamed_field):
+                                value = re.sub(r['re'], r['to'], value)
+                        if value != row[field]:
+                            logger.debug(f'Transform `{row[field]}` to `{value}`')
+    
+                        # Validate field values according to regex rules
+                        for r in rules.get('validate', []):
+                            if r.get('field') is None or re.match(r.get('field'), renamed_field):
+                                canonic_value, ok = validateValue(value, r.get('type'))
+                                if not ok or r.get('re', False) and not re.match(r.get('re'), value):
+                                    lvl = getLogLevel(r.get('log', 'warning'))
+                                    msg = f'Invalid {renamed_field}={value} (rule:{r.get("id","")})'
+                                    logger.log(lvl, msg)
+                                    if lvl == logging.CRITICAL:
+                                        raise Exception(msg)
+                                if r.get('unique', False):
+                                    unique_key = renamed_field + '::' + canonic_value.lower().replace(" ","")
+                                    if unique_idx.get(unique_key, False):
+                                        lvl = getLogLevel(r.get('log', 'warning'))
+                                        msg = f'Duplicate {renamed_field}={value} AND {unique_idx.get(unique_key)} (rule:{r.get("id","")})'
+                                        logger.log(lvl, msg)
+                                        if lvl == logging.CRITICAL:
+                                            raise Exception(msg)
+                                    else:
+                                        unique_idx[unique_key] = value
+
+                        # Custom actions
+                        for r in rules.get('custom', []):
+                            if r.get('field') is None or re.match(r.get('field'), renamed_field):
+                                if r.get('re') is None or re.search(r.get('re'), value):
+                                    action_set |= set([r['action']])
+    
+                        logger.debug(f'{renamed_field}={value}')
+                        output[renamed_field] = value
+
+                    for act in action_set:
+                        FunctionRegistry.exec(act)(output)
+ 
+                    logger.debug(f'INPUT: {row}')
+                    logger.debug(f'OUTPUT: {output}')
+                    csvwriter.writerow(output)
